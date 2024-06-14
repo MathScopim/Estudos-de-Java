7240ed274762b8b8908b096bf39bf0a2bd9b9dae
MathScopim <matheus.scopim@gmail.com>
2024-06-13 23:07:43 -0300

    Estudos de Java Dia 5

---
+++ Estudos De Java/Dia 5/src/MethodS.java
@@ -0,0 +1,11 @@
+public class MethodS {
+    public static void main(String[] args) {
+        productList("Laranja", 12.33);
+        productList("Banana", 17.50);
+
+    }
+    public static void productList(String productName, Double productValue) {
+        System.out.println(productName + " - " + productValue);
+    }
+
+}
