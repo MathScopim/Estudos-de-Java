1428a791025d63790d062ab4c04013f8be3b7e9a
MathScopim <matheus.scopim@gmail.com>
2024-06-27 21:32:23 -0300

    Estudos de Java D.I.O

---
+++ Estudos De Java/DioClass.java/src/Identation.java
@@ -0,0 +1,16 @@
+
+public class Identation {
+    public static void main(String[] args) {
+        // Deixa o código mais legível
+        System.out.println("Digite Seu nome: ");
+        int mediaFinal = 777;
+        if (mediaFinal < 6)
+            System.out.println("REPROVADO");
+        else if (mediaFinal == 6) {
+            System.out.println("PROVA MEDIANA"); }
+        else
+            System.out.println("APROVADO");
+
+
+    }
+}
---
+++ Estudos De Java/DioClass.java/src/NameClass.java
@@ -0,0 +1,9 @@
+public class NameClass {
+    public static void main(String[] args) {
+        String meuNome = "Matheus";
+        // Numeros Inteiros ( sem virgula )
+        int anoFabricacao = 1998;
+        // True False
+        boolean verdadeira = false;
+    }
+}
