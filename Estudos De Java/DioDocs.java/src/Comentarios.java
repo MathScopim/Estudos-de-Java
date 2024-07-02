d3d17a596c6a157c4c28ed39abae3e28e372a936
MathScopim <matheus.scopim@gmail.com>
2024-07-02 01:16:55 -0300

    Estudos de Java D.I.O 3

---
+++ Estudos De Java/DioDocs.java/src/Comentarios.java
@@ -0,0 +1,23 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+        * Eu sou um comentario
+        * Que posso ser mais detalhado
+        * Quanddo necessário
+        * Posso conter várias informações
+         */
+    }
+
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nivél de documentação
+     */
+
+    public void metodo() {
+    }
+}
+// Não amezina a linguagem
