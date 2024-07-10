361be9616e726969fb1d6120d913645577bc7cbe
MathScopim <matheus.scopim@gmail.com>
2024-07-09 22:21:50 -0300

    Estudos de Java D.I.O 10

---
+++ Estudos De Java/Package/src/escola.java
@@ -0,0 +1,11 @@
+package escola;
+public class escola {
+    public static void main(String[] args) {
+        aluno felipe = new aluno();
+        felipe.setNome("Felipe");
+        felipe.setIdade(8);_
+        System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade());
+
+
+    }
+}
