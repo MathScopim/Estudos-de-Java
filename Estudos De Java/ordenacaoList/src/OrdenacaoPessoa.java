85d48518f06ab9d37c6e7511fbb3de098f518d1f
MathScopim <matheus.scopim@gmail.com>
2024-07-16 22:58:54 -0300

    Estudos de Java D.I.O 16

---
+++ Estudos De Java/ordenacaoList/src/OrdenacaoPessoa.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenadorPorAltura() {
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 23, 2.01);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 34, 1.63);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 27, 1.92);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 32, 1.77);
+
+        System.out.println(ordenacaoPessoa.pessoaList);
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenadorPorAltura());
+    }
+}
